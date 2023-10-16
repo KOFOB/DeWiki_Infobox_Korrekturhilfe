444ff8e36907c26bf603547430b3dafec43bd64d
KOFOB <PatrickFH.Sonnentaggmail.com>
2023-10-16 07:30:46 +0200

    First simlple check for wikitables

--- simple_check.py
+++ simple_check.py
@@ -1,4 +1,6 @@
 # This class checks given wikitext if it contains a self-made infobox
+import re
+
 from extractor import extract_wikitext
 
 
@@ -9,6 +11,15 @@ class Checker:
 
     # Checks whether there is a raw wiki table in the article
     def check_for_table(self):
-        if self.wiki_text.contains("wikitable"):
+        copy = self.wiki_text.copy()
+
+        # Define a regular expression pattern to match "<math> ... </math>" to remove math, as there are
+        # also substrings of the form "{|" or "|}", which we want to ignore
+        pattern = r'<math>.*?</math>'
+
+        # Use re.sub() to replace the matched pattern with an empty string
+        copy = re.sub(pattern, '', copy)
+
+        if copy.contains("{|"):
             return True
         return False
